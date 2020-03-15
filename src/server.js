3fc8b51a6177a9257239a5deb54d7a437b03f7d4
Kyle Caprio <capriok7@yahoo.com>
2020-03-14 23:21:02 -0700

    touch

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const app = express()
 // const port = process.env.PORT || 9000
 // app.listen(port, () => console.log(`Server running on port: ${port}`))
 
+console.log = () => { }
+
 app.use(cors())
 app.use(express.json())
 
