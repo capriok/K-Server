188929f381c2d8b951a64b8c5bcb98b083c523e7
Kyle Caprio <capriok7@yahoo.com>
2020-05-08 18:04:27 -0700

    cors

--- src/server.js
+++ src/server.js
@@ -9,8 +9,20 @@ const app = express()
 const port = process.env.PORT || 9000
 app.listen(port, () => console.log(`Server running on port: ${port}`))
 
-app.use(cors())
 app.use(express.json())
+
+var whitelist = ['http://localhost:3000', 'https://sqlifting.netlify.app']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.options('*', cors(corsOptions))
 
 //CONNECT TO MONGODB
 const uri =
@@ -20,15 +32,15 @@ mongoose.set('useFindAndModify', false);
 const connection = mongoose.connection
 connection.once('open', () => console.log('MongoDB connected successfully'))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
-    return res.status(200).json({})
-  }
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   if (req.method === 'OPTIONS') {
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 
 //LEADERBOARD ENTRY ROUTE
 const leaderboardRouter = require('./routes/leaderboardEntry')
