4f370c8faf6d3b5ce6ba782fa065ce2dd9c8052e
Kyle Caprio <capriok7@yahoo.com>
2020-08-05 12:43:48 -0700

    config: ignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
   plugins: [
-    new webpack.IgnorePlugin(/^pg-native$/)
+    new webpack.IgnorePlugin(/^pg-native$/),
+    new webpack.IgnorePlugin(/^mongodb-client-encryption$/)
   ]
 }
