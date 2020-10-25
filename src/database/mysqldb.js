ce75f917c9471c495a42e37a65bfbfaebe239545
Kyle Caprio <capriok7@yahoo.com>
2020-10-24 23:19:06 -0700

    fix

--- src/database/mysqldb.js
+++ src/database/mysqldb.js
@@ -3,22 +3,22 @@ const HOST = process.env.MYSQLHOST
 const PASS = process.env.MYSQLPASS
 
 //RDS
-// const connection = mysql.createConnection({
-//   user: 'admin',
-//   password: PASS,
-//   database: 'sqlifting',
-//   host: HOST,
-//   port: 3306,
-//   multipleStatements: true
-// });
-
-//LOCAL
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  user: 'admin',
   password: PASS,
   database: 'sqlifting',
+  host: HOST,
+  port: 3306,
   multipleStatements: true
 });
+
+//LOCAL
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: PASS,
+//   database: 'sqlifting',
+//   multipleStatements: true
+// });
 
 module.exports = connection
